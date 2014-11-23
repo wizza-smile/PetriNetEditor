0ea2b482a4533bbbf1059e7ae7f60f053d54e8b2
Jonas Karsten <j.karsten@mail.de>
2014-11-23 10:12:54 +0100

    cleanup

--- PetriNetEditor.java
+++ PetriNetEditor.java
@@ -2,16 +2,15 @@ import model.*;
 import view.*;
 import controller.*;
 
-
-class PetriNetEditor {
+/**
+ * A class just to start the PetriNetEditor
+ */
+public class PetriNetEditor {
 
     public static void main(String[] args) {
         System.out.println("START");
-
         controller.GlobalController.startApplication();
-
     }
-
 
 }
 
